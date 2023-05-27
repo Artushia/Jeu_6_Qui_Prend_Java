b213c7c59daa4d2802e6b3dc5caa1f70cfac3af4
Artushia <arthurterisse@gmail.com>
2023-05-27 16:23:42 +0200

    - utilisation de scene builder pour les vues et réogarnisation complète des fichiers - Fichier GameView inutile pour l'instant avant modification ultérieur

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,10 +5,10 @@ module com.example.jeu_6_qui_prend_java {
     requires org.slf4j;  // Declare the SLF4J dependency
 
 
-    opens com.example.jeu_6_qui_prend_java to javafx.fxml;
-    exports com.example.jeu_6_qui_prend_java;
     exports com.example.jeu_6_qui_prend_java.Model;
-    exports com.example.jeu_6_qui_prend_java.ui;
-    exports com.example.jeu_6_qui_prend_java.ui.card;
+    exports com.example.jeu_6_qui_prend_java.Controller;
+    opens com.example.jeu_6_qui_prend_java.Controller to javafx.fxml;
     opens com.example.jeu_6_qui_prend_java.Model to javafx.fxml;
+    exports com.example.jeu_6_qui_prend_java.View;
+    opens com.example.jeu_6_qui_prend_java.View to javafx.fxml;
 }
